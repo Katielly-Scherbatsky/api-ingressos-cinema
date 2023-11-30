3a82f6c6ed6d7b87aaaf5f0ad138a4901157c582
Katielly <katiellyscherbatsky@gmail.com>
2023-11-30 14:05:04 -0400

    commit inicial

--- src/routes/base.js
+++ src/routes/base.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Configuração de rotas
 // Rota principal
 router.get("/", function (request, response) {
-  return response.send("API Funcionando...");
+  return response.send("API de Gestão Cinematográfica");
 });
 
 router.get("/Autor", function (request, response) {
@@ -12,12 +12,9 @@ router.get("/Autor", function (request, response) {
 });
 
 router.get("/sobre", function (repost, response) {
-  const info = {
-    nome: "Katielly Bordin Santos",
-    email: "katielly@gmail.com",
-    telefone: "(69) 99999-9999",
-  };
-  return response.json(info);
+  return response.send(
+    "A API de Gestão Cinematográfica simplifica o gerenciamento de cinemas, oferecendo funcionalidades como reservas de ingressos, programação de sessões, acompanhamento de vendas e análise de desempenho. Intuitiva e flexível, essa ferramenta permite que operadores de cinemas otimizem processos, proporcionando uma experiência cinematográfica sem complicações para seus clientes."
+  );
 });
 
 // Exportação da constante router código padrão
